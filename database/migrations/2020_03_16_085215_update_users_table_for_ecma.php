6681f0ffb48445d28e63e8d614d69bb835f54b7e
Maikel Zwart <maikel@eburon.media>
2020-03-26 11:10:48 +0100

    Update 2020_03_16_085215_update_users_table_for_ecma.php

--- database/migrations/2020_03_16_085215_update_users_table_for_ecma.php
+++ database/migrations/2020_03_16_085215_update_users_table_for_ecma.php
@@ -14,11 +14,12 @@ class UpdateUsersTableForEcma extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->renameColumn('name', 'first_name');
-            $table->string('last_name')->after('first_name');
+            $table->string('last_name')->after('name');
             $table->integer('admin_role')->default(0)->after('remember_token');
             $table->boolean('developer')->default(0)->after('admin_role');
             $table->boolean('active')->default(1)->after('developer');
+
+            $table->renameColumn('name', 'first_name');
         });
     }
 
