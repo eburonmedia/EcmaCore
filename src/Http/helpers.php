d84c48df174ae10ec98cbb651f1453617d0636f2
Maikel Zwart <maikel@eburon.media>
2020-06-29 12:03:15 +0200

    Update helpers.php

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -122,6 +122,12 @@ function adminTypes($type = null)
     }
 }
 
+function getYoutubeId($url)
+{
+    preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $url, $match);
+    return $match[1];
+}
+
 function isMobile()
 {
     $useragent = $_SERVER['HTTP_USER_AGENT'];
