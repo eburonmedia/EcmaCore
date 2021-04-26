c103f06f5d013a5d71de2ec16e17ed52dd25ec2f
Maikel Zwart <maikel@houseofcodes.nl>
2021-04-26 11:46:53 +0200

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="page-footer" class="bg-body-light">
-    <div class="content py-0 font-size-sm">
+    <div class="content py-0 font-size-sm text-right">
         <a class="font-w400" href="https://houseofcodes.nl" target="_blank"><span class="font-w700">HouseOfCodes</span></a> &copy; {{ date('Y') }} - {{ App::VERSION() }}
     </div>
 </footer>
